a6f8d7e14831ad96d2ec6e4c282cfb40d906af09
Johnathan Call <johnathancall@users.noreply.github.com>
2025-02-28 13:24:07 -0700

    Removing config.json from tracking, and getting a single playlist

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { readFileSync } from 'fs'
 const data = readFileSync('./config.json', 'utf-8')
 const config = JSON.parse(data)
 const token = config.token
+const playlist_ids = config.playlist_ids
 
 async function fetchWebApi(endpoint, method, body) {
 	const res = await fetch(`https://api.spotify.com/${endpoint}`, {
@@ -11,21 +12,22 @@ async function fetchWebApi(endpoint, method, body) {
 		},
 		method,
 		body:JSON.stringify(body)
-	});
-	return await res.json();
+	})
+	return await res.json()
 }
 
 async function getTopTracks(){
 	// Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
 	return (await fetchWebApi(
-		'v1/me/top/tracks?time_range=long_term&limit=5', 'GET'
-	)).items;
+		'v1/me/top/tracks?time_range=long_term&limit=10', 'GET'
+	)).items
 }
 
-const topTracks = await getTopTracks();
-console.log(
-	topTracks?.map(
-		({name, artists}) =>
-			`${name} by ${artists.map(artist => artist.name).join(', ')}`
-	)
-);
+async function getPlaylistItems(playlist_id){
+	return (await fetchWebApi(
+		`v1/playlists/${playlist_id}/tracks`, 'GET'
+	)).items
+}
+
+const topTracks = await getTopTracks()
+console.log(await getPlaylistItems(playlist_ids[0]))
