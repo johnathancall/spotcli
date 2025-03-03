832b07371bc47de25183b2b47a9fb383e2ccdce8
Johnathan Call <johnathancall@users.noreply.github.com>
2025-03-03 13:35:34 -0700

    Updated to request authtoken on every use

--- index.js
+++ index.js
@@ -2,8 +2,30 @@ import { readFileSync } from 'fs'
 
 const data = readFileSync('./config.json', 'utf-8')
 const config = JSON.parse(data)
-const token = config.token
-const base_playlist_ids = config.base_playlist_ids
+let token = ""
+
+async function getToken(clientId, clientSecret) {
+    const url = "https://accounts.spotify.com/api/token";
+    
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: new URLSearchParams({
+            grant_type: "client_credentials",
+            client_id: config.client_id,
+            client_secret: config.client_secret
+        })
+    });
+
+    if (!response.ok) {
+        throw new Error(`Error fetching token: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data.access_token;
+}
 
 async function fetchWebApi(endpoint, method, body) {
 	const res = await fetch(`https://api.spotify.com/${endpoint}`, {
@@ -17,7 +39,7 @@ async function fetchWebApi(endpoint, method, body) {
 }
 
 async function getBasePlaylistNames(){
-	return await Promise.all(base_playlist_ids.map(async (id) => {
+	return await Promise.all(config.base_playlist_ids.map(async (id) => {
 		let res = await fetchWebApi(`v1/playlists/${id}`, 'GET')
 		return {id: id, name: res.name}
 	}))
@@ -29,4 +51,5 @@ async function getPlaylistItems(playlist_id){
 	)).items
 }
 
+token = await getToken()
 console.log(await getBasePlaylistNames())
