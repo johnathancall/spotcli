adff314a19f1ed54cd55c67713253483a6569fc1
Johnathan Call <johnathancall@users.noreply.github.com>
2025-03-03 14:43:01 -0700

    Finished subplaylists & started clearing functionality

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const data = readFileSync('./config.json', 'utf-8')
 const config = JSON.parse(data)
 let token = ""
 
+// Get authtoken for use in all other operations
 async function getToken(clientId, clientSecret) {
     const url = "https://accounts.spotify.com/api/token";
     
@@ -27,6 +28,7 @@ async function getToken(clientId, clientSecret) {
     return data.access_token;
 }
 
+// Catch-all fetch web API function
 async function fetchWebApi(endpoint, method, body) {
 	const res = await fetch(`https://api.spotify.com/${endpoint}`, {
 		headers: {
@@ -38,20 +40,104 @@ async function fetchWebApi(endpoint, method, body) {
 	return await res.json()
 }
 
-async function getBasePlaylistNames(playlist_ids){
-	return await Promise.all(playlist_ids.map(async (id) => {
+
+// Fetching playlist info
+async function getPlaylistNames(playlist_ids){
+	const playlists = await Promise.all(playlist_ids.map(async (id) => {
 		let res = await fetchWebApi(`v1/playlists/${id}`, 'GET')
+		console.log(res.name)
 		return {id: id, name: res.name}
 	}))
+
+	return new Map(playlists.map(({name, id}) => [name, id]));
 }
 
-async function getPlaylistItems(playlist_id){
-	return (await fetchWebApi(
-		`v1/playlists/${playlist_id}/tracks`, 'GET'
-	)).items
+async function getSubPlaylists(combined_playlist_info) {
+	// Getting sub-playlists
+	let playlists = []
+	combined_playlist_info.forEach((id, name) => {
+		// Parse playlist name
+		let tokens = name.split(' ')
+		let h_str = tokens[0].slice(1)
+		let e_str = tokens[1].slice(1)
+
+		// Get H values
+		let h_vals = []
+		h_vals[0] = parseInt(h_str[0])
+		if(h_str.length > 1) {
+			let h_max = parseInt(h_str[2])
+			while(h_vals[h_vals.length - 1] < h_max) {
+				h_vals.push(h_vals[h_vals.length - 1] + 1)
+			}
+		}
+
+
+		// Get E values
+		let e_vals = []
+		e_vals[0] = parseInt(e_str[0])
+		if(e_str.length > 1) {
+			let e_max = parseInt(e_str[2])
+			while(e_vals[e_vals.length - 1] < e_max) {
+				e_vals.push(e_vals[e_vals.length - 1] + 1)
+			}
+		}
+
+		// Get corresponding playlists
+		
+		playlists[name] = []
+		h_vals.forEach((h_val) => {
+			e_vals.forEach((e_val) => {
+				let prefix = 'H'
+				playlists[name].push(prefix.concat(h_val, 'E', e_val))
+			})
+		})
+	})
+
+	return playlists
 }
 
-// Calls begin here
+
+// Update playlists
+
+async function removeFromPlaylist(track_id, playlist_id) {
+	await fetchWebApi(`v1/playlists/${playlist_id}/tracks`, 'DELETE', [track_id])
+}
+
+async function clearPlaylist(playlist_id) {
+	/* Step 1: get all track IDs inside this playlist */
+
+	// Fetch first batch of tracks
+	let tracks_json = await fetchWebApi(`v1/playlists/${playlist_id}/tracks`, 'GET')
+	let tracks_info = tracks_json.items
+
+	// Handle first batch of tracks
+	let track_ids = []
+	tracks_info.forEach((track_info) => {
+		track_ids.push(track_info.track.id)
+	})
+
+	// Get any further batches of tracks
+
+	tracks_json = await fetchWebApi(`v1/playlists/${playlist_id}/tracks`, 'GET')
+	tracks_info = tracks_json.items
+	console.log(tracks_json)
+	
+}
+
+async function mergePlaylists() {
+
+}
+
+/* Calls begin here */
+
 token = await getToken()
 
+let basePlaylistInfo = await getPlaylistNames(config.base_playlist_ids)
 
+let combinedPlaylistInfo = await getPlaylistNames(config.combined_playlist_ids)
+
+let subPlaylists = await getSubPlaylists(combinedPlaylistInfo)
+
+config.combined_playlist_ids.forEach((id) => {
+	clearPlaylist(id)
+})
